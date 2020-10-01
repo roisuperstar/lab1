3b7a90601c353b968faaeb2ed3eee08561d84bd0
roisuperstar <trantuanit95@gmail.com>
2020-10-01 23:28:12 +0700

    lab1

---
+++ diyles2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int a=4*4*4*4;
+	printf("%d\n", a);
+	
+	int b=23.34;
+	printf("%d\n", b);
+	
+	int c=10, d=c+c++;
+	printf("%d\n", d);
+	
+	int e=-5, f=-e;
+	printf("%d\n", f);
+}
---
+++ ex4les2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int x, y;
+	x=5; y=2;
+	printf("The integer are : %d & %d\n", x, y);
+	printf("The addition gives : %d\n", x + y);
+	printf("The subtraction gives : %d\n", x - y );
+	printf("The multuplication gives : %d\n", x*y);
+	printf("The division gives : %d\n", x/y);
+	printf("The modulus gives : %d\n", x%y);
+	
+	getchar();
+	
+}
